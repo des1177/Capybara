4376c549541f58793fdff9ea169b1364b73efdab
des1177 <yinwu@ucdavis.edu>
2021-07-30 15:23:55 -0700

    Update note.java

--- note.java
+++ note.java
@@ -3,3 +3,5 @@
 // using GoLang
 // 哪里吵
 
+// Black Box Testing is a software testing method in which the internal structure/ design/ implementation of the item 
+// being tested is not known to the tester. White Box Testing is a software testing method in which the internal structure/ design/ implementation of the item being tested is known to the tester.
